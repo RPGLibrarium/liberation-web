3ba199dd37e857f7700dbe627a3a516fd938f951
Richard Z <git+liberation@richardz.de>
2022-03-20 16:40:57 +0100

    do not show edit buttons to unauthenticated users

--- src/app/js/rpgsystems.js
+++ src/app/js/rpgsystems.js
@@ -10,70 +10,77 @@ ROUTER
   .on('systems/:id', args=>PAGE._RENDER(nestWrap('rpgsystem', loadRpgSystemWithTitles), PAGE.system, args));
 
 function onDisplayRpgSystems(pageNode) {
-  pageNode.querySelectorAll('.systems tr[data-rpgsystemid]').forEach((row) => {
+  let systemsTable = pageNode.querySelector('table.systems');
+  if (systemsTable) {
+    const editable = systemsTable.matches('.editable');
     let editing = false;
-    let rowInputs = {};
-    row.querySelectorAll('input:not([type=submit]), select').forEach(el => rowInputs[el.name] = el);
-    let rowValues = {};
-    row.querySelectorAll('.value-text[data-name]').forEach(el => rowValues[el.getAttribute('data-name')] = el);
 
-    function abortEditing() {
-      if (!editing) return;
-      editing = false
-      row.classList.remove('editing')
-      // Reset inputs
-      Object.values(rowInputs).forEach(input => {
-        input.value = input.getAttribute('value')
-      });
-    }
+    systemsTable.querySelectorAll('tr[data-rpgsystemid]').forEach((row) => {
+      if (editable) {
+        let rowInputs = {};
+        row.querySelectorAll('input:not([type=submit]), select').forEach(el => rowInputs[el.name] = el);
+        let rowValues = {};
+        row.querySelectorAll('.value-text[data-name]').forEach(el => rowValues[el.getAttribute('data-name')] = el);
 
-    row.querySelector('.rowbutton[value=edit]')
-      .addEventListener('click', event => {
+        function abortEditing() {
+          if (!editing) return;
+          editing = false
+          row.classList.remove('editing')
+          // Reset inputs
+          Object.values(rowInputs).forEach(input => {
+            input.value = input.getAttribute('value')
+          });
+        }
+
+        row.querySelector('.rowbutton[value=edit]')
+          .addEventListener('click', event => {
+            event.preventDefault()
+            event.stopPropagation();
+            if (editing) return;
+            editing = true
+            row.classList.add('editing')
+          });
+        row.querySelector('.rowbutton[value=delete]')
+          .addEventListener('click', event => {
+            event.preventDefault()
+            event.stopPropagation();
+            // TODO: edit code here
+          });
+        row.querySelector('.rowbutton[value=save]')
+          .addEventListener('click', event => {
+            event.preventDefault()
+            event.stopPropagation();
+            updateRpgSystem({id: Number(row.getAttribute('data-rpgsystemid')), system:{
+              name: rowInputs.name.value,
+              shortname: rowInputs.shortname.value !== "" ? rowInputs.shortname.value : null,
+            }}).then(system => {
+              console.debug('updated rpgsystem', system);
+              rowValues.name.textContent = system.name;
+              rowValues.shortname.textContent = system.shortname !== null ? system.shortname : '';
+              rowInputs.name.setAttribute('value', system.name);
+              rowInputs.shortname.setAttribute('value', system.shortname !== null ? system.shortname : '');
+              abortEditing();
+            }).catch(err => {
+              console.error('updating rpgsystem failed', err);
+              //TODO: nicer error message
+              alert(`Fehler ${err}`);
+            })
+          });
+        row.querySelector('.rowbutton[value=abort]')
+          .addEventListener('click', event => {
+            event.preventDefault();
+            event.stopPropagation();
+            abortEditing();
+          });
+      }
+      row.addEventListener('click', event => {
+        if(editing || event.target.matches('a,button,select,input')){ return; }
         event.preventDefault()
-        event.stopPropagation();
-        if (editing) return;
-        editing = true
-        row.classList.add('editing')
+        let systemid = row.getAttribute('data-rpgsystemid');
+        ROUTER.navigate('systems/' + encodeURIComponent(systemid));
       });
-    row.querySelector('.rowbutton[value=delete]')
-      .addEventListener('click', event => {
-        event.preventDefault()
-        event.stopPropagation();
-        // TODO: edit code here
-      });
-    row.querySelector('.rowbutton[value=save]')
-      .addEventListener('click', event => {
-        event.preventDefault()
-        event.stopPropagation();
-        updateRpgSystem({id: Number(row.getAttribute('data-rpgsystemid')), system:{
-          name: rowInputs.name.value,
-          shortname: rowInputs.shortname.value !== "" ? rowInputs.shortname.value : null,
-        }}).then(system => {
-          console.debug('updated rpgsystem', system);
-          rowValues.name.textContent = system.name;
-          rowValues.shortname.textContent = system.shortname;
-          rowInputs.name.setAttribute('value', system.name);
-          rowInputs.shortname.setAttribute('value', system.shortname);
-          abortEditing();
-        }).catch(err => {
-          console.error('updating rpgsystem failed', err);
-          //TODO: nicer error message
-          alert(`Fehler ${err}`);
-        })
-      });
-    row.querySelector('.rowbutton[value=abort]')
-      .addEventListener('click', event => {
-        event.preventDefault();
-        event.stopPropagation();
-        abortEditing();
-      });
-    row.addEventListener('click', event => {
-      if(editing || event.target.matches('a,button,select,input')){ return; }
-      event.preventDefault()
-      let systemid = row.getAttribute('data-rpgsystemid');
-      ROUTER.navigate('systems/' + encodeURIComponent(systemid));
     });
-  });
+  }
 }
 
 export function loadRpgSystems() {
